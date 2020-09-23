87ef94de97700fe04ccb7a5091d571a60886ffec
kremir <kremir@o2.pl>
2020-09-23 10:15:00 +0200

    initialize project with some data populated

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+create table person (
+    id int not null primary key,
+    name varchar(255) not null,
+    location varchar(255),
+    birth_date timestamp
+);
---
+++ src/test/java/krenc/mirek/jdbcjpa/JdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package krenc.mirek.jdbcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
